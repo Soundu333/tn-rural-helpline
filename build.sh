4744adf15a0a3d566e22f29b97e829865b2b6ee2
Soundharya <msoundharya40@gmail.com>
2026-03-02 21:55:38 +0530

    add superuser creation

--- build.sh
+++ build.sh
@@ -3,3 +3,12 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='mainadmin').exists():
+    u = User.objects.create_superuser('mainadmin', 'msoundharya40@gmail.com', 'Admin@1234')
+    u.role = 'main_admin'
+    u.save()
+    print('Superuser created!')
+"
