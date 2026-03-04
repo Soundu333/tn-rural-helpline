8e6f2fe2773b6d48277a59a4de2e7c47bacecd09
Soundharya <msoundharya40@gmail.com>
2026-03-04 10:20:21 +0530

    remove captcha urls

--- rural_helpline/urls.py
+++ rural_helpline/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('captcha/', include('captcha.urls')),
     path('accounts/', include('accounts.urls')),
     path('complaints/', include('complaint_mgmt.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
