1074a925dd96ed472f90551288154cca34a9b9e3
Soundharya <msoundharya40@gmail.com>
2026-03-09 10:16:25 +0530

    fix urls track complaint

--- complaint_mgmt/urls.py
+++ complaint_mgmt/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register_complaint, name='register_complaint'),
     path('my-complaints/', views.my_complaints, name='my_complaints'),
-    path('track/', views.track_complaint, name='track_complaint'),
     path('get-subcategories/', views.get_subcategories, name='get_subcategories'),
     path('admin/complaint/<int:id>/', views.admin_complaint_detail, name='admin_complaint_detail'),
 ]
